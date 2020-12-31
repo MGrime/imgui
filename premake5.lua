af8dfc36feb3c69e556fba191d25c940d5816bae
Michael Grime <35151019+MGrime@users.noreply.github.com>
2020-12-31 18:05:23 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -18,7 +18,7 @@ project "ImGui"
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-		"imgui_tables.cpp"
+		"imgui_tables.cpp",
         "imgui_demo.cpp"
     }
 
